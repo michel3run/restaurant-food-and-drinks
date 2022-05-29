b99df55c7dcc6c5789a3c90920e944140a091d54
michel3run <michel.umlg@gmail.com>
2022-05-29 16:07:23 +0200

    post api no implemnt

--- src/app/service/api/api.service.ts
+++ src/app/service/api/api.service.ts
@@ -6,10 +6,19 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  url = 'http://localhost:8080/event'
+  url = 'http://localhost:8080/user'
   constructor(private http:HttpClient) { }
 
   getAllUser():Observable<listaUser[]>{
     return this.http.get<listaUser[]>(this.url);
   }
+
+  getUser(id:string):Observable<listaUser>{
+    return this.http.get<listaUser>(this.url +"/"+id)
+  }
+
+  postUser(user:string,pass:string){
+     this.http.post<any>(this.url+'/userImport',{usuario: user , password : pass})
+  }
+  
 }
--- src/events.js
+++ src/events.js
@@ -5,7 +5,7 @@ function createRouter(db) {
   const owner = '';
 
   // the routes are defined here
-  router.get('/event', function (req, res, next) {
+  router.get('/user', function (req, res, next) {
     db.query(
       'SELECT * FROM prueba ',
       (error, results) => {
@@ -14,6 +14,36 @@ function createRouter(db) {
           res.status(500).json({status: 'error'});
         } else {
           res.status(200).json(results);
+        }
+      }
+    );
+  });
+
+  router.get('/user/:id', function (req, res, next) {
+    db.query(
+      'SELECT * FROM prueba where user=? ',
+      [req.params.id],
+      (error, results) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({status: 'error'});
+        } else {
+          res.status(200).json(results);
+        }
+      }
+    );
+  });
+
+  router.post('/user/userImport', (req, res, next) => {
+    db.query(
+      'INSERT INTO prueba  VALUES (?,?)',
+      [req.body.usuario,req.body.password],
+      (error) => {
+        if (error) {
+          console.error(error);
+          res.status(500).json({status: 'error'});
+        } else {
+          res.status(200).json({status: 'ok'});
         }
       }
     );
