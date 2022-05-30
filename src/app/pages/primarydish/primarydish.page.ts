e801dcbe62f2014e0479841a440f16511739c591
michel3run <michel.umlg@gmail.com>
2022-05-31 00:49:45 +0200

    Cookies casi

--- src/app/pages/primarydish/primarydish.page.ts
+++ src/app/pages/primarydish/primarydish.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/service/api/api.service';
 import { MenuService } from 'src/app/service/menu/menu.service';
+import { Cookie } from 'ng2-cookies';
 
 @Component({
   selector: 'app-primarydish',
@@ -10,9 +11,22 @@ import { MenuService } from 'src/app/service/menu/menu.service';
 export class PrimarydishPage implements OnInit {
   primeros = [];
   cuenta = [];
-  total: number = 0
-  constructor(private api: ApiService, private menu: MenuService) { }
-
+  cookies: Object;
+  keys: Array<string>;
+  cName: string;
+  cValue: string;
+  rName: string;
+  checkName: string;
+  constructor(private api: ApiService, private menu: MenuService) {
+    this.update()
+    console.log(Cookie.get('unidad-0'))
+    console.log(this.keys)
+    
+    
+  }
+  ngAfterViewInit(){
+    console.log("finasl")
+  }
   ngOnInit() {
     this.menu.showMenu = true
     this.api.getAllProduct().subscribe((data) => {
@@ -20,13 +34,47 @@ export class PrimarydishPage implements OnInit {
         this.primeros.push(item.nombre + " " + item.precio + "€")
         this.cuenta.push(Number(item.precio))
       }
+
+     
     })
+  
   }
+
+
+  update() {
+    this.cookies = Cookie.getAll();
+    this.keys = Object.keys(this.cookies);
+  }
+
+  addCookie(cName: string, cValue: string) {
+    console.log('Adding: ', cName, cValue);
+    Cookie.set(cName, cValue);
+    this.update();
+  }
+  removeCookie(rName: string) {
+    console.log('Removing: ', rName);
+    Cookie.delete(rName);
+    this.update();
+  }
+  removeAll() {
+    console.log('Removing all cookies');
+    Cookie.deleteAll();
+    this.update();
+  }
+  checkCookie() {
+    console.log('Checking: ', this.checkName);
+    console.log(Cookie.check(this.checkName));
+    window.alert('Check cookie ' + this.checkName + ' returned ' + Cookie.check(this.checkName));
+  }
+
+
   add(i:string ) {
     var unidad = document.getElementById('unidad-' + i);
     unidad!.innerText = String(Number(unidad!.innerText) + 1);
-    this.total += this.cuenta[Number(i)] 
     this.menu.cuentaTotal += this.cuenta[Number(i)]
+    this.addCookie('unidad-' + i,unidad!.innerText)
+    this.update()
+    
   }
 
   delete(i: string) {
@@ -35,6 +83,15 @@ export class PrimarydishPage implements OnInit {
       unidad!.innerText = String(Number(unidad!.innerText) - 1);
       this.menu.cuentaTotal -= this.cuenta[Number(i)]
     }
+    this.addCookie('unidad-' + i,unidad!.innerText)
+    this.update()
+  }
 
+  load(){
+    for(let item of this.keys){
+      console.log(item)
+      var unidad = document.getElementById(item);
+      unidad!.innerText = Cookie.get(item);
+    }
   }
 }
