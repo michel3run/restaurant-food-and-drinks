c8ad1a0a36e25e1ebb3af56ca0a6704f41aff891
michel3run <michel.umlg@gmail.com>
2022-06-02 01:18:05 +0200

    fix userID defaut 0

--- src/app/service/menu/menu.service.ts
+++ src/app/service/menu/menu.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MenuService {
-  userID:number
+  userID:number=0
   ticket= []
   platos={}
   showMenu:boolean =false;
